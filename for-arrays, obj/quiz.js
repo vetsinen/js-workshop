7bde148ca4d0db4659bafc623dc80efeec16a43e
vetsinen <vetsinen@gmail.com>
2021-12-23 23:59:45 +0200

    quiz added

---
+++ for-arrays, obj/quiz.js
@@ -0,0 +1,42 @@
+ let arr = [1,7,2,8,9, 4, 3]
+
+ // for (let i=0;i<10;i++){
+//     console.log(i)
+// }
+//
+// for (let i=0;i<arr.length;i++){
+//     console.log(i, arr[i])
+// }
+
+// for (let i=arr.length-1;i>=0;i--){
+//     console.log(arr[i])
+// }
+
+// for (let i=0;i<arr.length;i+=2){
+//     arr[i]=2*arr[i]
+//     if (i+1<arr.length){
+//         arr[i+1] = arr[i+1]/2
+//     }
+// }
+// console.log(arr)
+
+ // let l = arr.length - 1
+ // for (let i = 0; i < arr.length / 2; i++) {
+ //     let tmp = arr[i]
+ //     arr[i] = arr[l - i]
+ //     arr[l - i] = tmp
+ // }
+ // console.log(arr)
+
+// let i = 1
+// let a = 1
+// while (i<=11){
+//     console.log(a)
+//     a*=2
+//     i++
+// }
+
+ // for (let i = 1; i < 11; i++)
+ //     for (let j = 1; j < 11; j++) {
+ //         console.log(`${i} * ${j} = ${i * j}`)
+ //     }
