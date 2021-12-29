e09b5475163d63dc6ff1a52a53e9f8f345e69b3b
vetsinen <vetsinen@gmail.com>
2021-12-29 11:25:27 +0200

    before lec06, paer 2

--- objects/08-func in object.js
+++ objects/08-func in object.js
@@ -1,16 +1,17 @@
+const event1 = {name:"Swarm Learning", date:"2021-08-30", fee: 42}
+console.log(event1)
+
 function Event(name,start,end){//!Upcase
     this.name = name
     this.start = start
     this.end = end
     this.duration = function (){//function in object
+        return  Math.abs(new Date(this.end) - new Date(start)) / 36e5
     }
 }
 
-const event1 = new Event("Digital Вівторок","2021-08-31","2021-08-31")
-console.log(event1)
-
-const event2 = {name:"Swarm Learning", date:"2021-08-30", fee: 42}
-console.log(event2)
+const event2 = new Event("Digital Вівторок","2021-08-28","2021-08-31")
+console.log(event2.duration())
 
 const location = {"place": " INVERIA Event Space", "city": "Kyiv"}
 
