1bac262b22f31f5b24da0a121a16f634e1f482a5
vetsinen <vetsinen@gmail.com>
2022-01-22 14:53:38 +0200

    js dom examples 3

--- webjs/cssSelector.js
+++ webjs/cssSelector.js
@@ -3,7 +3,7 @@ const p = document.querySelector('p')
 p.onclick = function (){
     console.log(p.style.fontSize)//different from css
     let newFont = 2 + parseFloat(p.style.fontSize)
-    p.style.fontSize = `${newFont}px`
+    p.style.fontSize = newFont+`px`
 }
 
 
--- webjs/iterate.js
+++ webjs/iterate.js
@@ -1,11 +1,17 @@
 const list = document.getElementsByTagName('ul')[0]
-console.log(list)
 
 for (let i=0;i<list.children.length; i++){
-    console.log(i, ord(i))
-    if (!ord(i)){
+    console.log(i, simple(i))
+    if (simple(i)){
         list.children.item(i).style.color = 'green'
     }
 }
 
-function ord(i){return i%2 ===0}
+function ord(i) {return i%2 ===0}
+
+function simple (num){
+    if (num<2) return false
+    for (let i=2; i<num; i++)
+        if (num%i===0) return false
+    return true
+}
