23efef16e7aa6d20da1eb12a96ffe5802fd09441
vetsinen <vetsinen@gmail.com>
2022-01-25 14:48:16 +0200

    car request added, fix1

--- callbacks, promises, async/00.js
+++ callbacks, promises, async/00.js
@@ -2,7 +2,6 @@ const axios = require("axios")
 
 async function getData(){
     let rez = await axios('https://vetsinen.pythonanywhere.com/')
-    //console.log(rez.data)
     for (let car of rez.data.data) console.log(car)
 }
 
