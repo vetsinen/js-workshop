e66da6ec54b7e4b495dac52be1e52e93e609d26c
vetsinen <vetsinen@gmail.com>
2022-01-28 20:46:15 +0200

    attached to new servers

--- dom-async etc/02-rooms.js
+++ dom-async etc/02-rooms.js
@@ -1,15 +1,15 @@
-const localUrl = 'http://127.0.0.1:5000/rooms/'
+const localUrl = 'http://10.10.150.75:5000/rooms/'
 const webUrl = 'https://vetsinen.pythonanywhere.com/rooms/'
 const rooms = document.getElementById("rooms")
 const district = document.getElementById('district')
 
 function addLiItem(item){
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode(item.street+' - '+item.price));
+    li.appendChild(document.createTextNode(item.adress+' - '+item.price));
     rooms.appendChild(li);
 }
 
-async function getData(district='pechersky'){
+async function getData(district='Оболонь'){
     let response = await fetch(localUrl+district)
     if (response.ok) {
         let rez = await response.json()
